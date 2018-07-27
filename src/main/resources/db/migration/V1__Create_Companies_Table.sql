9c73e20089ddefc5e3757229016ee08f0bcaec6b
BaleLin <bale.lin@oocl.com>
2018-07-27 08:31:10 +0800

    companies那里的sql修复一些问题

--- src/main/resources/db/migration/V1__Create_Companies_Table.sql
+++ src/main/resources/db/migration/V1__Create_Companies_Table.sql
@@ -1,4 +1,4 @@
-CREATE TABLE `tw_company` (
+CREATE TABLE `Companies` (
   `id`                 BIGINT       NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `name`         VARCHAR(20)  NOT NULL,
   `created_date` TIMESTAMP DEFAULT NOW()
